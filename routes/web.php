7553d13406ea9633d7178d388d139c5284652d1f
Arif Febriana <febrianaarif7@gmail.com>
2023-07-09 00:41:54 +0700

    menambahkan beberapa macam contoh method routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,40 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-// Generate Application key
+// Generate Application key | GET method
 $router->get('/key', function ()  {
     return str_random(32);
+});
+
+// Example GET method
+$router->get('/get', function ()  {
+    return 'This GET Method';
+});
+
+//-----------------------------------
+// Example Method Verb
+//-----------------------------------
+// Example POST method
+$router->post('/post', function ()  {
+    return 'This POST Method';
+});
+
+// Example PUT method
+$router->post('/put', function ()  {
+    return 'This PUT Method';
+});
+
+// Example PATCH method
+$router->post('/PATCH', function ()  {
+    return 'This PATCH Method';
+});
+
+// Example delete method
+$router->post('/delete', function ()  {
+    return 'This delete Method';
+});
+
+// Example delete method
+$router->post('/options', function ()  {
+    return 'This options Method';
 });
