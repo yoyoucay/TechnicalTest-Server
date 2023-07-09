00baaf6dc8ff7e72fd0c1e56e41da5bedab7341a
Arif Febriana <febrianaarif7@gmail.com>
2023-07-09 23:24:02 +0700

    Membuat beberapa function di pada "AuthController" - Register, dengan ditambahkan dengan construction dgn return json. - Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,48 @@
 namespace App\Http\Controllers;
 use \Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class AuthController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+{   
     public function __construct()
     {
         # Middleware
         // $this->middleware('verify', ['except'=> ['GetRoomID']]);
     }
-    //
+
+    public function Register(Request $request){
+
+        #Pengambilan
+        $fullname = $request->input('fullname');
+        $email = $request->input('email');
+        $password = Hash::make($request->input('password'));
+
+        
+        $register ::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        if ($register) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Berhasil meregistrasi akun!',
+                'data' => $register
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Gagal meregistrasi akun!',
+                'data' => $register
+            ], 400);
+        }
+        
+    }
+
+    public function Login(Request $request){
+        
+    }
 }
